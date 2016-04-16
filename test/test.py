fa8cc5b61ea668aa277426a6158b1aa56337ae44
SunDwarf <sun@veriny.tf>
2016-04-16 12:46:50 +0100

    Update test.py to use the validated format.

--- test/test.py
+++ test/test.py
@@ -1,9 +1,14 @@
 import pyte
 
-bc = [pyte.tokens.LOAD_CONST, 0, 0,
+# Create a new consts value.
+consts = pyte.create_validated("Hello, world!")
+# New varnames values
+varnames = pyte.create_validated()
+
+bc = [pyte.load.LOAD_CONST(consts[0]),
       pyte.tokens.RETURN_VALUE]
 
-func = pyte.compile(bc, ["Hello, world!"], [])
+func = pyte.compile(bc, consts, varnames)
 
 print(func.__code__.co_code)
 
