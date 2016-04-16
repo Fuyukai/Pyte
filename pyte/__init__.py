a68bdd5aaf3358fe38704f0d49213d35391bd1bb
SunDwarf <sun@veriny.tf>
2016-04-16 12:19:47 +0100

    Add first superclasses

--- pyte/__init__.py
+++ pyte/__init__.py
@@ -4,3 +4,9 @@ Pyte package file, import some useful stuff from other functions.
 
 from .compiler import compile
 from . import tokens
+from . import superclasses
+
+
+# Helper for creating new validated lists.
+def create_validated(*args) -> superclasses.PyteAugmentedArgList:
+    return superclasses.PyteAugmentedArgList(*args)
--- pyte/superclasses.py
+++ pyte/superclasses.py
@@ -1,3 +1,51 @@
 """
 Superclasses for various Pyte things.
 """
+import functools
+
+
+class _PyteOp(object):
+    """
+    This is a superclass for an opcode object, I.E an operation like `LOAD_FAST`.
+    """
+
+    # How wide this operation is (in bytes)
+    op_width = 0
+
+    def to_bytes(self) -> bytes:
+        """
+        Produces a byte string representing the `co_code` of this operator.
+        """
+        raise NotImplementedError
+
+
+class _PyteAugmentedValidator(object):
+    """
+    An augmented validator ensures that the bytecode objects do not segfault when the bytecode is compiled and ran,
+    by validating the arguments.
+    """
+    def __init__(self, index, get_partial):
+        self.index = index
+        self.partial = get_partial
+
+    def get(self):
+        return self.partial()
+
+
+class PyteAugmentedArgList(list):
+    """
+    This is a superclass for an augmented argument list.
+
+    This is not actually subclassed, but used directly in an alias.
+
+    Instead of providing items to use, it provides validators that are validated in `compile`.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __getitem__(self, item):
+        # Create a partial to get the list item.
+        part = functools.partial(super().__getitem__, item)
+        # Return a new _PyteAugmentedValidator.
+        return _PyteAugmentedValidator(item, part)
+
