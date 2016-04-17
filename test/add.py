1352f7cbf9d0bd20f0227f7f16acd5ce28c23501
SunDwarf <sun@veriny.tf>
2016-04-17 14:46:41 +0100

    Make add example smaller to show inline addition inside an IF op

--- test/add.py
+++ test/add.py
@@ -6,14 +6,12 @@ import pyte
 # Create a new consts value.
 consts = pyte.create_consts(None, 1, 2, 3, "Equal!")
 # New varnames values
-varnames = pyte.create_varnames("saved")
+varnames = pyte.create_varnames()
 # Create names (for globals)
 names = pyte.create_names("print")
 
 bc = [
-    consts[1] + consts[2],
-    pyte.ops.STORE_FAST(varnames[0]),
-    pyte.ops.IF(conditions=[varnames[0] == consts[3]], body=[[pyte.ops.CALL_FUNCTION(names[0], consts[4])]]),
+    pyte.ops.IF(conditions=[consts[1] + consts[2] == consts[3]], body=[[pyte.ops.CALL_FUNCTION(names[0], consts[4])]]),
     pyte.ops.END_FUNCTION(consts[0])
 ]
 
